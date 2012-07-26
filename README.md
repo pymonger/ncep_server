c21577b0a3bad0ce2c0f523cf4f789f4dc43eda1
Gerald Manipon <pymonger@gmail.com>
2012-07-26 01:04:41 -0700

    initial import

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ ncep_server
 ===========
 
 Puppet module to install HDF5, NetCDF4, numpy, scipy, netcdf4, and various other packages on Ubuntu 12.04.
+
+Installation
+------------
+Copy ncep_server to /etc/puppet/modules then run:
+
+puppet apply -e "include ncep_server"
---
+++ manifests/init.pp
@@ -0,0 +1,130 @@
+define pip($ensure = installed) {
+  case $ensure {
+    installed: {
+      exec { "pip install $name":
+        path => "/usr/local/bin:/usr/bin:/bin",
+      }
+    }
+    latest: {
+      exec { "pip install --upgrade $name":
+        path => "/usr/local/bin:/usr/bin:/bin",
+      }
+    }
+    default: {
+      exec { "pip install $name==$ensure":
+        path => "/usr/local/bin:/usr/bin:/bin",
+      }
+    }
+  }
+}
+
+class ncep_server {
+
+  package { 'libfreetype6-dev':
+    ensure => present,
+  }
+
+  package { 'libpng12-dev':
+    ensure => present,
+  }
+
+  package { 'python': 
+    ensure => present,
+  }
+
+  package { 'python-dev': 
+    ensure => present,
+  }
+
+  package { 'python-virtualenv': 
+    ensure => present,
+  }
+
+  package { 'python-pip': 
+    ensure => present,
+  }
+
+  package { 'python-numpy':
+    ensure => installed,
+  }
+
+  package { 'python-matplotlib':
+    ensure => installed,
+  }
+
+  package { 'python-matplotlib-data':
+    ensure => installed,
+  }
+
+  package { 'python-mpltoolkits.basemap':
+    ensure => installed,
+  }
+
+  package { 'python-mpltoolkits.basemap-data':
+    ensure => installed,
+  }
+
+  package { 'python-tables':
+    ensure => installed,
+  }
+
+  package { 'python-numexpr':
+    ensure => installed,
+  }
+
+  package { 'python-scipy':
+    ensure => installed,
+  }
+
+  package { 'zlib':
+    provider => dpkg,
+    ensure   => present,
+    source   => "puppet:///modules/ncep_server/zlib_1.2.7-1_amd64.deb",
+  }
+
+  package { 'hdf5':
+    provider => dpkg,
+    ensure   => present,
+    source   => "puppet:///modules/ncep_server/hdf5_1.8.9-1_amd64.deb",
+    require  => Package['zlib'],
+  }
+
+  package { 'netcdf':
+    provider => dpkg,
+    ensure   => present,
+    source   => "puppet:///modules/ncep_server/netcdf_4.1.3-1_amd64.deb",
+    require  => Package['hdf5'],
+  }
+
+  pip { 'netcdf4':
+    ensure  => installed,
+    require => [Package['python-numpy'], Package['netcdf']],
+  }
+  
+  exec { '/sbin/ldconfig': 
+    refreshonly => true, 
+    alias       => 'ldconfig', 
+    subscribe   => Package['netcdf'],
+  } 
+  
+  define inputrc ($user = $title, $home) {
+    file { "$home/.inputrc":
+      ensure  => file,
+      mode    => 0644,
+      owner   => $user,
+      content => '#"\e[5~": history-search-backward #for "page up" key
+  #"\e[6~": history-search-forward  #for "page down" key
+  "\e[A": history-search-backward
+  "\e[B": history-search-forward
+  ',
+    }
+  }
+  
+  inputrc { 'root':
+    home => '/root',
+  }
+  
+  inputrc { 'sflops':
+    home => '/home/sflops',
+  }
+}
