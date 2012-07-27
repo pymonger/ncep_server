9a8b5a7bc1677fcf6e7da6885acc1ff620e2a28a
Gerald Manipon <pymonger@gmail.com>
2012-07-27 11:12:56 -0700

    Changed operation account to ncep. Reorganized dependencies in manifest and added comments.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,7 @@
+#####################################################
+# pip provider
+#####################################################
+
 define pip($ensure = installed) {
   case $ensure {
     installed: {
@@ -18,7 +22,70 @@ define pip($ensure = installed) {
   }
 }
 
+
+#####################################################
+# .inputrc provider
+#####################################################
+
+define inputrc ($user = $title, $home) {
+  file { "$home/.inputrc":
+    ensure  => file,
+    mode    => 0644,
+    owner   => $user,
+    content => '#"\e[5~": history-search-backward #for "page up" key
+#"\e[6~": history-search-forward  #for "page down" key
+"\e[A": history-search-backward
+"\e[B": history-search-forward
+',
+  }
+}
+  
+
+#####################################################
+# ncep_server class
+#####################################################
+
 class ncep_server {
+
+  #####################################################
+  # create groups and users
+  #####################################################
+  $user = 'ncep'
+  $group = 'ncep'
+
+  group { $group:
+    ensure     => present,
+    gid        => '1002',
+  }
+
+  user { $user:
+    ensure     => present,
+    uid        => '1002',
+    gid        => '1002',
+    shell      => '/bin/bash',
+    home       => "/home/$user",
+    managehome => true,
+    require    => Group[$group],
+  }
+
+
+  #####################################################
+  # add .inputrc to users' home
+  #####################################################
+
+  inputrc { 'root':
+    home => '/root',
+  }
+  
+  inputrc { $user:
+    home    => "/home/$user",
+    require => User[$user],
+  }
+
+
+  #####################################################
+  # install packages
+  #####################################################
 
   package {
     'libfreetype6-dev': ensure => present;
@@ -50,11 +117,21 @@ class ncep_server {
     'tomcat6-docs': ensure => installed;
   }
 
+
+  #####################################################
+  # refresh ld cache
+  #####################################################
+
   exec { 'ldconfig': 
     command     => '/sbin/ldconfig',
     refreshonly => true, 
   } 
   
+
+  #####################################################
+  # install home baked packages for HDF5 and NetCDF4
+  #####################################################
+
   package { 'zlib':
     provider => dpkg,
     ensure   => present,
@@ -87,32 +164,34 @@ class ncep_server {
     notify   => Exec['ldconfig'],
   }
 
+
+  #####################################################
+  # use pip to install NetCDF4 python module
+  #####################################################
+
   pip { 'netcdf4':
     ensure  => installed,
     require => [Package['python-numpy'], Package['netcdf']],
   }
   
+
+  #####################################################
+  # install home baked packages and configuration for 
+  # BES and OPeNDAP handlers
+  #####################################################
+
   package { 'libdap':
     provider => dpkg,
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/libdap_3.11.3-1_amd64.deb",
     require  => Package['netcdf'],
-    notify   => Exec['ldconfig'],
   }
 
-  file { '/etc/ld.so.conf.d/bes.conf':
-    ensure  => file,
-    mode    => 0644,
-    content => '/usr/local/lib/bes\n',
-    notify   => Exec['ldconfig'],
-  }
-    
   package { 'bes':
     provider => dpkg,
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/bes_3.10.2-1_amd64.deb",
     require  => Package['libdap'],
-    notify   => File['/etc/ld.so.conf.d/bes.conf'],
   }
 
   package { 'dap-server':
@@ -120,7 +199,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/dap-server_4.1.2-1_amd64.deb",
     require  => Package['bes'],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'netcdf-handler':
@@ -128,7 +206,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/netcdf-handler_3.10.1-1_amd64.deb",
     require  => Package['dap-server'],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'freeform-handler':
@@ -136,7 +213,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/freeform-handler_3.8.4-1_amd64.deb",
     require  => Package['dap-server'],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'hdf4-handler':
@@ -144,7 +220,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/hdf4-handler_3.9.4-1_amd64.deb",
     require  => Package['dap-server'],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'hdf5-handler':
@@ -152,7 +227,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/hdf5-handler_2.0.0-1_amd64.deb",
     require  => Package['dap-server'],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'fileout_netcdf':
@@ -160,7 +234,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/fileout-netcdf_1.1.2-1_amd64.deb",
     require  => Package['dap-server'],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'gateway-module':
@@ -168,7 +241,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/gateway-module_1.1.0-1_amd64.deb",
     require  => [Package['dap-server'], Package['libcurl4-openssl-dev']],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'xml_data-handler':
@@ -176,7 +248,6 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/xml-data-handler_1.0.2-1_amd64.deb",
     require  => Package['dap-server'],
-    notify   => Exec['ldconfig'],
   }
 
   package { 'csv-handler':
@@ -184,59 +255,91 @@ class ncep_server {
     ensure   => present,
     source   => "/etc/puppet/modules/ncep_server/files/csv-handler_1.0.2-1_amd64.deb",
     require  => Package['dap-server'],
-    notify   => Exec['ldconfig'],
   }
+
+  #####################################################
+  # create ld conf file for /usr/local/lib/bes  
+  # and refresh cache
+  #####################################################
+
+  file { '/etc/ld.so.conf.d/bes.conf':
+    ensure  => file,
+    mode    => 0644,
+    content => '/usr/local/lib/bes\n',
+    require => [
+                Package['netcdf-handler'],
+                Package['freeform-handler'],
+                Package['hdf4-handler'],
+                Package['hdf5-handler'],
+                Package['fileout_netcdf'],
+                Package['gateway-module'],
+                Package['xml_data-handler'],
+                Package['csv-handler'],
+               ],
+    notify  => Exec['ldconfig'],
+  }
+    
+
+  #####################################################
+  # install BES startup script and custom configs
+  #####################################################
 
   file { '/etc/init.d/besctl':
     ensure  => file,
     mode    => 0755,
     source  => 'puppet:///modules/ncep_server/besctl',
-    require => Package['bes'],
+    require => [
+                Package['bes'],
+                File['/etc/ld.so.conf.d/bes.conf'],
+               ]
   }
 
   file { '/usr/local/etc/bes/bes.conf':
     ensure  => file,
     mode    => 0644,
     source  => 'puppet:///modules/ncep_server/bes.conf',
-    require => Package['bes'],
+    require => File['/etc/init.d/besctl'],
   }
     
   file { '/usr/local/etc/bes/modules/dap.conf':
     ensure  => file,
     mode    => 0644,
     source  => 'puppet:///modules/ncep_server/dap.conf',
-    require => Package['bes'],
+    require => File['/usr/local/etc/bes/bes.conf'],
   }
     
+
+  #####################################################
+  # create directories need by BES with proper perms
+  #####################################################
+
   file { ['/usr/local/var',
           '/usr/local/var/run',
           '/usr/local/var/run/bes']:
-    ensure => directory,
-    owner  => 'root',
-    group  => 'gdgps',
-    mode   => 0775,
+    ensure  => directory,
+    owner   => 'root',
+    group   => $group,
+    mode    => 0775,
+    require => File['/usr/local/etc/bes/modules/dap.conf'],
   }
+
+
+  #####################################################
+  # ensure BES service starts
+  #####################################################
 
   service { 'besctl':
-    ensure => running,
-    enable => true,
+    ensure     => running,
+    enable     => true,
     hasrestart => true,
-    hasstatus => true,
-    require => [
-                Package['bes'], 
-                File['/etc/init.d/besctl'], 
-                File['/usr/local/etc/bes/bes.conf'],
-                File['/usr/local/etc/bes/modules/dap.conf'],
-                File['/usr/local/var/run/bes'],
-               ],
+    hasstatus  => true,
+    require    => File['/usr/local/var/run/bes'],
   }
 
-  file { 'opendap':
-    path => '/var/lib/tomcat6/webapps/opendap.war',
-    ensure => present,
-    source => 'puppet:///modules/ncep_server/opendap.war',
-    require => Package['tomcat6'],
-  }
+  #####################################################
+  # ensure directories exist for opendap WAR and 
+  # install opendap WAR to tomcat's webapps
+  #####################################################
 
   file { ['/var/lib/tomcat6/content',
           '/var/lib/tomcat6/content/opendap',
@@ -245,16 +348,33 @@ class ncep_server {
     require => Package['tomcat6'],
   }
 
+  file { '/var/lib/tomcat6/webapps/opendap.war':
+    ensure => present,
+    source => 'puppet:///modules/ncep_server/opendap.war',
+    require => [
+                Package['tomcat6'],
+                Service['besctl'],
+                File['/var/lib/tomcat6/content/opendap/logs'],
+               ],
+  }
+
+
+  #####################################################
+  # ensure tomcat service starts
+  #####################################################
+
   service { 'tomcat6':
     ensure => running,
     enable => true,
     hasrestart => true,
     hasstatus => true,
-    require => [
-                File['opendap'],
-                File['/var/lib/tomcat6/content/opendap/logs'],
-               ],
+    require => File['/var/lib/tomcat6/webapps/opendap.war'],
   }
+
+
+  #####################################################
+  # ensure various hyrax configurations are soft-linked
+  #####################################################
 
   file { '/var/lib/tomcat6/content/opendap/olfs.xml':
     ensure => link,
@@ -266,57 +386,6 @@ class ncep_server {
     ensure => link,
     target => '/var/lib/tomcat6/webapps/opendap/initialContent/catalog.xml',  
     require => Service['tomcat6'],
-  }
-
-  define inputrc ($user = $title, $home) {
-    file { "$home/.inputrc":
-      ensure  => file,
-      mode    => 0644,
-      owner   => $user,
-      content => '#"\e[5~": history-search-backward #for "page up" key
-  #"\e[6~": history-search-forward  #for "page down" key
-  "\e[A": history-search-backward
-  "\e[B": history-search-forward
-  ',
-    }
-  }
-  
-  inputrc { 'root':
-    home => '/root',
-  }
-  
-  inputrc { 'sflops':
-    home    => '/home/sflops',
-    require => User['sflops'],
-  }
-
-  inputrc { 'gdgps':
-    home    => '/home/gdgps',
-    require => User['gdgps'],
-  }
-
-  user { 'sflops':
-    ensure     => present,
-    uid        => '1000',
-    gid        => '1000',
-    shell      => '/bin/bash',
-    home       => '/home/sflops',
-    managehome => true,
-  }
-
-  user { 'gdgps':
-    ensure     => present,
-    uid        => '1001',
-    gid        => '1001',
-    shell      => '/bin/bash',
-    home       => '/home/gdgps',
-    managehome => true,
-    require    => Group['gdgps'],
-  }
-
-  group { 'gdgps':
-    ensure     => present,
-    gid        => '1001',
   }
 
 }
